eb4d57d755a9643c9f684b971fd09594ae276174
John Wright Stanly <jwstanly@gmail.com>
2020-08-03 19:12:52 -0400

    transitioning to Controller class

--- Controller.cpp
+++ Controller.cpp
@@ -1,17 +1,15 @@
 #include "Controller.hpp"
-
 #include "SFML/Window/Joystick.hpp"
 
 #include <iostream>
 #include <string>
-#include <unistd.h>
-#include <map>
+
 
 Controller::Controller(unsigned int port){
     this->port = port;
     sf::Joystick::update();
     if(sf::Joystick::isConnected(port)) {
-        std::cout << "Connected" << std::endl;
+        std::cout << "Connection succeeded" << std::endl;
     }
     else{
         std::cout << "Connection failed you silly" << std::endl;
@@ -44,20 +42,37 @@ float Controller::getThrust(){
     else return thrust/100;
 }
 
+float Controller::getPitch(){
+  float pitch = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
+  if(pitch<20) return 0;
+  else return pitch/100;
+}
+
 float Controller::getRoll(){
     float roll = sf::Joystick::getAxisPosition(0, sf::Joystick::X);
     if(roll<20) return 0;
     else return roll/100;
 }
 
-float Controller::getPitch(){
-    float pitch = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
-    if(pitch<20) return 0;
-    else return pitch/100;
-}
 
 float Controller::getYaw(){
     float yaw = sf::Joystick::getAxisPosition(0, sf::Joystick::X);
     if(yaw<20) return 0;
     else return yaw/100;
+}
+
+float Controller::getMotor(){
+    return this->getThrust();
+}
+
+float Controller::getElevator(){
+    return this->getPitch();
+}
+
+float Controller::getAileron(){
+    return this->getRoll();
+}
+
+float Controller::getRudder(){
+    return this->getYaw();
 }
--- Controller.hpp
+++ Controller.hpp
@@ -3,14 +3,23 @@
 class Controller{
     public:
         Controller(unsigned int port); //constructor input port number (default 0)
-        ~Controller();
+        ~Controller(); //deconstructor
+
         bool getButton(unsigned int buttonNumber); //input button ID, returns whether it's pressed or not
         bool getButton(std::string buttonString); //input button name, returns whether it's pressed or not
-        float getThrust();  //returns thurst value from 0.2-1
+
+        float getThrust(); //returns thrust value from 0.2-1
         float getPitch(); //returns pitch value from 0.2-1
         float getRoll(); //returns roll value from 0.2-1
         float getYaw(); //returns yaw value from 0.2-1
+
+        float getMotor();
+        float getElevator();
+        float getAileron();
+        float getRudder();
+
     private:
         unsigned int port;
+        unisgned int channels;
         std::string buttonName[11] = {"A","B","X","Y","LB","RB","MENU","START","XBOX","LSB","RSB"};
 };
