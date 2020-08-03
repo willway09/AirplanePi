855a647f5375fa4195d303dbf79b62955cede7cf
John Wright Stanly <jwstanly@gmail.com>
2020-08-03 17:11:39 -0400

    added main for testing

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "SFML/Window/Joystick.hpp"
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <map>
+
+#include "Controller.hpp"
+
+int main(){
+    Controller xbox(0);
+}
