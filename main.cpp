5ceb808478e2fce1c1e8700205da6b20a79f0943
John Wright Stanly <jwstanly@gmail.com>
2020-08-03 19:44:49 -0400

    general controller updates

--- main.cpp
+++ main.cpp
@@ -7,5 +7,14 @@
 #include "Controller.hpp"
 
 int main(){
-    Controller xbox(0);
+    Controller xbox(3,0);
+
+    float timeOut = 0.1;
+    while(true){
+        std::cout << "Thrust: " << xbox.getThrust() << " ";
+        std::cout << "Pitch: " << xbox.getPitch() << " ";
+        std::cout << "Roll: " << xbox.getRoll() << " ";
+        std::cout << "Yaw: " << xbox.getYaw() << " " << std::endl;
+        usleep(timeOut*1000000);
+    }
 }
