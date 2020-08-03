1cd24276eaaa0b549c0543ab7d7e56e5b6bd52a8
John Wright Stanly <jwstanly@gmail.com>
2020-08-03 17:12:59 -0400

    controller updates

--- Controller.hpp
+++ Controller.hpp
@@ -5,6 +5,7 @@ class Controller{
         Controller(unsigned int port);
         ~Controller();
         bool getA();
+        bool getB();
     private:
         unsigned int port;
         std::string buttonName[11] = {"A","B","X","Y","LB","RB","MENU","START","XBOX","LSB","RSB"};
