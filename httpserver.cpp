871367255d1ac3f39613e18e0351dbb9c637060c
John Wright Stanly <jwstanly@gmail.com>
2020-08-20 15:29:00 -0400

    started server side of HTTP client

--- httpserver.cpp
+++ httpserver.cpp
@@ -18,209 +18,23 @@
 #include <unordered_map>
 #include <fstream>
 
-//User-defined headers
-#include "onewire.hpp"
-
 //Structs for sharing groups of values between main() and threads
-struct PwmValues {
-	int pwmPercent = 0;
-	int frequency = 500;
-	pthread_mutex_t pwmControlMutex = PTHREAD_MUTEX_INITIALIZER;
+struct Axes{
+	float thrust = 0;
+	float pitch = 0;
+	float roll = 0;
+	float yaw = 0;
+	pthread_mutex_t axesControlMutex = PTHREAD_MUTEX_INITIALIZER;
 };
 
-struct EnvironmentValues {
-	double temperature = 0;
-	double humidity = 0;
-	pthread_mutex_t environmentControlMutex = PTHREAD_MUTEX_INITIALIZER;
+struct ValuesContainer{
+	struct Axes* axes;
 };
-
-struct ValuesContainer {
-	struct PwmValues* pwmValues;
-	struct EnvironmentValues* enviroValues;
-};
-
-//Control PWM signal
-void* pwmControl(void* vargp) {
-
-	//Convert void vargp pointer to appropriate PwmValues pointer
-	struct PwmValues* pwmValues = (struct PwmValues*) vargp;
-
-	//Open GPIO chip
-	struct gpiod_chip* chip = gpiod_chip_open("/dev/gpiochip0");
-
-	//If openining fails, exit thread
-	if(!chip) {
-		std::cerr << "Error opening chip" << std::endl;
-		return NULL;
-	}
-
-	//Get specific PIN from chip
-	struct gpiod_line* line = gpiod_chip_get_line(chip, 23); //CHANGE PWM OUTPUT PIN HERE - currently 23
-
-	//If opening pin fails, exit thread
-	if(!line) {
-		gpiod_chip_close(chip);
-		std::cerr << "Error opening line" << std::endl;
-		return NULL;
-	}
-
-
-	//Create configuration struct for pin - set to output type;
-	struct gpiod_line_request_config outputConfig = { "pwmControl", GPIOD_LINE_REQUEST_DIRECTION_OUTPUT, 0 };
-
-
-	//Configure GPIO PIN, and if failure, exit thread
-	if(gpiod_line_request(line, &outputConfig, 0) == -1) {
-		gpiod_chip_close(chip);
-		std::cerr << "Error requesting line configuration" << std::endl;
-		return NULL;
-	}
-
-	
-
-	while(true) { //Main PWM loop
-	
-		//Declare local variables to copy from pwmValues struct, which is volatile
-		int localPercent, localFrequency;
-		
-		//Lock mutex so that pwmValues can be read
-		pthread_mutex_lock(&(pwmValues->pwmControlMutex));
-	
-			localPercent = pwmValues->pwmPercent;
-			localFrequency = pwmValues->frequency;
-
-		//Unlock mutex
-		pthread_mutex_unlock(&(pwmValues->pwmControlMutex));
-
-		//Prevent a divide-by-zero error
-		if(localFrequency == 0) localFrequency = 1;
-
-
-		//Compute the delays, in microseconds, for the PWM pulse
-		double periodOn = (((double)localPercent / 100 ) / localFrequency) * 1E6;
-		double periodOff = (((double)(100 - localPercent) / 100 ) / localFrequency) * 1E6;
-
-		if(localPercent == 0) { //Condition for 0% duty cycle
-			gpiod_line_set_value(line, 0);
-			usleep((long)(periodOff + periodOn));
-		} else if(localPercent == 100) { //Condition for 100% duty cycle
-			gpiod_line_set_value(line, 1);
-			usleep((long)(periodOff + periodOn));
-		} else { //PWM control
-			gpiod_line_set_value(line, 1);
-			usleep((long) periodOn);
-			gpiod_line_set_value(line, 0);
-			usleep((long) periodOff);;
-		}
-		
-		//Note that usleep() both delays for an (approximately) appropriate ammount of time
-		//and yields resources back to the CPU, preventing the infinite while loop
-		//from spinlocking the CPU
-
-	}
-}
-
-//Get values from X-DTHS-P temperature/humidity sensor
-void* environmentControl(void* vargp) {
-	
-	//Convert void vargp pointer to appropriate EnvironmentValues pointer
-	struct EnvironmentValues* enviroValues = (struct EnvironmentValues*) vargp;
-	
-	//Open GPIO chip
-	struct gpiod_chip* chip = gpiod_chip_open("/dev/gpiochip0");
-	
-	//If opening fails, exit thread
-	if(!chip) {
-		std::cerr << "Error opening chip" << std::endl;
-		return NULL;
-	}
-	
-	//Get specific PIN from chip
-	struct gpiod_line* line = gpiod_chip_get_line(chip, 24); //CHANGE ONEWIRE DATA PIN HERE - currently 24
-	
-	//If opening pin fails, exit thread
-	if(!line) {
-		gpiod_chip_close(chip);
-		std::cerr << "Error opening line" << std::endl;
-		return NULL;
-	}
-	
-	
-	//Configure GPIO PIN, and if failure, exit thread
-	//oneWireOutputConfig defined in onewire.hpp
-	if(gpiod_line_request(line, &oneWireOutputConfig, 0) == -1) {
-		gpiod_chip_close(chip);
-		std::cerr << "Error requesting line configuration" << std::endl;
-		return NULL;
-	}
-	
-	
-	//Note that reset(), sendByte(), and readByte() are all defined and implemented in onewire.hpp
-	
-	while(true) { //Main sensor loop
-	
-		reset(line); //Send reset pulse
-	
-		
-		sendByte(line, 0xCC); //Address all devices (only one on line)
-			
-				//This step appears to be necessary - sets ROS bit, which empirically seems necesary based on
-				//observations of functionality of ControlByWeb products
-			
-				sendByte(line, 0xCC); //Write Conditional Search Register
-				
-				//Register 0x008D
-				sendByte(line, 0x8D);
-				sendByte(line, 0x00);
-			
-				sendByte(line, 0x04); //Write value 0x04
-			
-			
-		reset(line); //Send reset pulse
-	
-		
-		sendByte(line, 0xcc); //Address all devices (only one on line)
-			sendByte(line, 0xF5); //Channel Access read
-		
-			//Read relevant values
-			unsigned char deviceCode = readByte(line);
-				
-			unsigned char tempLsb = readByte(line);
-			unsigned char tempMsb = readByte(line);
-			
-			unsigned char humLsb = readByte(line);
-			unsigned char humMsb = readByte(line);
-			
-			unsigned char crc = readByte(line);
-			
-			double localTemp = std::round(100 * ( -45 + 175. * ((tempMsb << 8) | tempLsb) / ((1 << 16) - 1) )) / 100;
-			double localHum = std::round(100 * (100. * ((humMsb << 8) | humLsb) / ((1 << 16) - 1))) / 100;
-			
-		
-		reset(line); //Reset line
-		
-		//Add conditional check for CRC - only update if CRC of bytes matches CRC sent
-		
-		pthread_mutex_lock(&(enviroValues->environmentControlMutex));
-		
-			enviroValues->temperature = localTemp;
-			enviroValues->humidity = localHum;
-		
-		pthread_mutex_unlock(&(enviroValues->environmentControlMutex));
-		
-		
-		sleep(3); //Sensor manual recommends delays of 2 seconds between readings,
-		          //or else usage will cause self-heating - 3 second delay is fine
-		          //Manual: https://www.controlbyweb.com/accessories/x-dths-p-users-manual.pdf
-	}
-	
-}
-
 
 std::string mainPage() { //Return the main web page, for user interfacing
 
 	std::ifstream indexFile("index.html"); //Open index.html, which is the main GUI webpage file
-	
+
 	std::string responseContent(""); //Declare responseContent, which is the non-header component of the response
 
 	if(indexFile.is_open()) { //If the file opened successfully
@@ -232,11 +46,11 @@ std::string mainPage() { //Return the main web page, for user interfacing
 		}
 
 		indexFile.close();
-		
+
 	} else { //Else send error message
 		responseContent += "<b>Could not open index.html</b>";
 	}
-	
+
 
 	//Create HTTP response
 	std::string response = "HTTP/1.1 200 OK\r\n";
@@ -265,48 +79,46 @@ bool isNumber(std::string& value) {
 std::string statePage(std::unordered_map<std::string, std::string>& params, void* data) {
 
 	//Convert void data pointer to appropriate PwmValues pointer
-	struct PwmValues* pwmValues = ((struct ValuesContainer*) data)->pwmValues;
-	
-	//Convert void data pointer to appropriate EnvironmentValues pointer
-	struct EnvironmentValues* enviroValues = ((struct ValuesContainer*) data)->enviroValues;
+	struct Axes* axes = ((struct ValuesContainer*) data)->axes;
 
-	//Declare localPercent, an integer representation of the parameter value, and set if percent is an integer
-	int localPercent = -1;
-	if(isNumber(params["percent"]))
-		localPercent = std::stoi(params["percent"]);
+	int localThrust = -1;
+	if(isNumber(params["thrust"]))
+		localThrust = std::stoi(params["thrust"]);
 
+	int localPitch = -1;
+	if(isNumber(params["pitch"]))
+		localPitch = std::stoi(params["pitch"]);
 
-	int localFrequency = -1;
-	if(isNumber(params["frequency"]))
-		localFrequency = std::stoi(params["frequency"]);
+	int localRoll = -1;
+	if(isNumber(params["roll"]))
+		localRoll = std::stoi(params["roll"]);
+
+	int localYaw = -1;
+	if(isNumber(params["yaw"]))
+		localYaw = std::stoi(params["yaw"]);
 
 
 	//Lock mutex briefly to update values
-	pthread_mutex_lock(&(pwmValues->pwmControlMutex));
-	
-		if(localPercent != -1)
-			pwmValues->pwmPercent = localPercent;
-		localPercent = pwmValues->pwmPercent;
-	
-		if(localFrequency != -1)
-			pwmValues->frequency = localFrequency;
-		localFrequency = pwmValues->frequency;
-		
+	pthread_mutex_lock(&(axes->axesControlMutex));
+
+		if(localThrust != -1)
+			axes->thrust = localThrust;
+		localThrust = axes->thrust;
+
+		if(localThrust != -1)
+			axes->pitch = localPitch;
+		localPitch = axes->pitch;
+
+		if(localRoll != -1)
+			axes->roll = localRoll;
+		localRoll = axes->roll;
+
+		if(localYaw != -1)
+			axes->yaw = localYaw;
+		localYaw = axes->yaw;
+
 	//Unlock mutex
-	pthread_mutex_unlock(&(pwmValues->pwmControlMutex));
-	
-	
-	double localTemp;
-	double localHum;
-	
-	//Lock mutex briefly to copy values
-	pthread_mutex_lock(&(enviroValues->environmentControlMutex));
-	
-		localTemp = enviroValues->temperature;
-		localHum = enviroValues->humidity;
-	
-	//Unlock mutex
-	pthread_mutex_unlock(&(enviroValues->environmentControlMutex));
+	pthread_mutex_unlock(&(axes->axesControlMutex));
 
 	std::ostringstream responseContent;
 	responseContent.precision(2);
@@ -317,32 +129,32 @@ std::string statePage(std::unordered_map<std::string, std::string>& params, void
 
 	responseContent << "<data>";
 		responseContent << "\n";
-	
+
 		//Generate percent response XML
-		responseContent << "<percent>";
-		responseContent << localPercent;
-		responseContent << "</percent>";
+		responseContent << "<thrust>";
+		responseContent << std::fixed << localThrust;
+		responseContent << "</thrust>";
 		responseContent << "\n";
-		
+
 		//Generate frequency response XML
-		responseContent << "<frequency>";
-		responseContent << localFrequency;
-		responseContent << "</frequency>";
+		responseContent << "<pitch>";
+		responseContent << std::fixed << localPitch;
+		responseContent << "</pitch>";
 		responseContent << "\n";
-		
+
 		//Generate temperature response XML
-		
-		responseContent << "<temperature>";
-		responseContent << std::fixed << localTemp;
-		responseContent << "</temperature>";
+
+		responseContent << "<roll>";
+		responseContent << std::fixed << localRoll;
+		responseContent << "</roll>";
 		responseContent << "\n";
-		
+
 		//Generate humidity response XML
-		responseContent << "<humidity>";
-		responseContent << std::fixed << localHum;
-		responseContent << "</humidity>";
+		responseContent << "<yaw>";
+		responseContent << std::fixed << localYaw;
+		responseContent << "</yaw>";
 		responseContent << "\n";
-		
+
 		responseContent << std::defaultfloat;
 
 	responseContent << "</data>";
@@ -373,38 +185,24 @@ std::string evaluateRequest(std::string file, std::unordered_map<std::string, st
 	//Scalable place to add handlers for different file requests
 	//Currently, only root page and state.xml are valid
 	//If file does not match any listings, return a basic HTTP 404 error page
-	
+
 	if(file.compare("/") == 0) {
 		return mainPage();
 	} else if(file.compare("/state.xml") == 0) {
 		return statePage(params, data);
 	} else if(file.compare("/index.html") == 0) { //Redundancy for more intuitive access
-		return mainPage();	
+		return mainPage();
 	} else {
 		return errorPage();
 	}
 }
 
+
 int main() {
-	
-	//Declare struct for sharing values between server and threads
-	struct PwmValues pwmValues;
-	struct EnvironmentValues enviroValues;
-	
-	struct ValuesContainer valuesContainer = { &pwmValues, &enviroValues };
-	
-	
-	//Create thread for PWM control
-	pthread_t pwmTid;
-	pthread_create(&pwmTid, NULL, pwmControl, &pwmValues);
-	
-	
-	//Create thread for environment sensor control
-	pthread_t enviroTid;
-	pthread_create(&enviroTid, NULL, environmentControl, &enviroValues);
-	
-	
-	
+
+	struct Axes axes;
+	struct ValuesContainer valuesContainer = { &axes };
+
 	//Declare variables for sockets
 	unsigned int s;
 	struct sockaddr_in local;
@@ -445,7 +243,7 @@ int main() {
 
 	//Main loop
 	while(true) {
-		
+
 		//Declare client
 		struct sockaddr_in client;
 		unsigned int clientLength = sizeof(client);
@@ -465,7 +263,7 @@ int main() {
 		//Populate request string based on HTTP content of values read from client connection
 		std::string request;
 		for(int count = read(client_val, buffer, buffSize - 1); count > 0; count = read(client_val, buffer, buffSize - 1)) { //buffSize - 1, to maintain \0 terminator
-			
+
 			//If buffer isn't completely full, add \0 terminator after last byte of data
 			if(count != buffSize - 1) {
 				buffer[count] = '\0';
@@ -520,7 +318,7 @@ int main() {
 
 		//Question mark separates file component of URL from parameter component
 		int questionPos = URL.find("?");
-		
+
 		//Declare string to hold file component of URL
 		std::string file;
 
